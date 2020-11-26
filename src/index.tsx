3f4190574e680ecbe80bef3ff134342368c807e0
Ricardo Andres Vejarano Garcia <ricardo.vejarano@globant.com>
2020-11-25 21:18:03 -0500

    login page added

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Login from './pages/login/login.page'
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route exact path='/login' component={App} />
+      <Route exact path='/' component={Login} />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ src/pages/login/login.page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './login.css';
+
+function Login() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    LOGIN!
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default Login;
