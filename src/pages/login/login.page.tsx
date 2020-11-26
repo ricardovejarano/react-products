5fd9194e53016703d603de264d79b471b1a42da4
Ricardo Andres Vejarano Garcia <ricardo.vejarano@globant.com>
2020-11-25 21:41:02 -0500

    login

--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -7,9 +7,22 @@ function Login() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    LOGIN!
-                </p>
+                <div className="card">
+                    <div className="card-body">
+                        <form>
+                            <div className="form-group">
+                                <label htmlFor="emailInput">Email</label>
+                                <input type="email" className="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="passwordInput">Password</label>
+                                <input type="password" className="form-control" id="passwordInput" placeholder="Enter password" />
+                            </div>
+                            <button type="submit" className="btn btn-primary mr-1">Login</button>
+                            <button type="submit" className="btn btn-primary ml-1">Register</button>
+                        </form>
+                    </div>
+                </div>
             </header>
         </div>
     );
